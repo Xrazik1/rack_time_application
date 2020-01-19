60cd0eb181d511c1ada06a9c0464d21c2dbcbd8f
Nikolay <Xrazik@yandex.ru>
2020-01-19 23:37:05 +0300

    feat: time app finished

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 class TimeHandler
-
+  VALID_TIME_KEYS = %w[year month day hour minute second].freeze
 
   def initialize
     @response_code = 200
@@ -17,5 +17,45 @@ class TimeHandler
     ]
   end
 
+  def valid_params?(params)
+    return false unless params.instance_of?(Array)
 
+    (params - VALID_TIME_KEYS).empty?
+  end
+
+  def handle_query(query)
+    params = query.split('=').last.split('%2C')
+
+    valid_params?(params) ? handle_time(params) : handle_bad_format(params)
+  end
+
+  def handle_time(params)
+    converted = convert_time(params)
+    time = Time.new
+    string_time = ''
+
+    converted.each_with_index do |converted_time, i|
+      string_time += (i + 1) == converted.length ? converted_time.to_s : "#{converted_time}-"
+    end
+
+    @response_body = ["#{time.strftime(string_time)}\n"]
+    @response_code = 200
+  end
+
+  def handle_bad_format(params)
+    @response_code = 400
+    @response_body = ["Unknown time format #{params}\n"]
+  end
+
+  def convert_time(params)
+    params.map do |time|
+      if %w[day month].include? time
+        "%#{time.split('').first}"
+      elsif %w[minute second year].include? time
+        "%#{time.split('').first.upcase}"
+      elsif time == 'hour'
+        '%I'
+      end
+    end
+  end
 end
