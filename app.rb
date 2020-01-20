b2443e94122ed90e62dd608bfb35515a282e21d0
Nikolay <Xrazik@yandex.ru>
2020-01-20 14:17:16 +0300

    fix: Time formatter moved to foreign class

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
-class TimeHandler
-  VALID_TIME_KEYS = %w[year month day hour minute second].freeze
+require_relative 'time_formatter'
 
+class App
   def initialize
     @response_code = 200
     @response_headers = {'Content-type' => 'text/plain'}
@@ -8,11 +8,10 @@ class TimeHandler
   end
 
   def call(env)
-    if env['REQUEST_PATH'] == '/time'
+    if valid_request_path? env['REQUEST_PATH']
       handle_query(env['QUERY_STRING'])
     else
-      @response_code = 404
-      @response_body = ["Request path #{env['REQUEST_PATH']} does not exists\n"]
+      handle_bad_request_path(env['REQUEST_PATH'])
     end
 
     [
@@ -22,41 +21,28 @@ class TimeHandler
     ]
   end
 
-  def valid_params?(params)
-    return false unless params.instance_of?(Array)
-
-    (params - VALID_TIME_KEYS).empty?
+  def valid_request_path?(path)
+    path == '/time'
   end
 
   def handle_query(query)
     params = query.split('=').last.split('%2C')
 
-    valid_params?(params) ? handle_time(params) : handle_bad_format(params)
+    if TimeFormatter.valid_format?(params)
+      @response_code = 200
+      @response_body = ["#{TimeFormatter.new.time_by_format(params)}\n"]
+    else
+      handle_bad_format(params)
+    end
   end
 
-  def handle_time(params)
-    converted = convert_time(params)
-    time = Time.new
-    string_time = converted.join('-')
-
-    @response_body = ["#{time.strftime(string_time)}\n"]
-    @response_code = 200
+  def handle_bad_request_path(path)
+    @response_code = 404
+    @response_body = ["Request path #{path} does not exists\n"]
   end
 
   def handle_bad_format(params)
     @response_code = 400
     @response_body = ["Unknown time format #{params}\n"]
-  end
-
-  def convert_time(params)
-    params.map do |time|
-      if %w[day month].include? time
-        "%#{time.split('').first}"
-      elsif %w[minute second year].include? time
-        "%#{time.split('').first.upcase}"
-      elsif time == 'hour'
-        '%I'
-      end
-    end
   end
 end
